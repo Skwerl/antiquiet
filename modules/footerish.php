06cb194579aa2f77dd70f96dc9cbd003fd584f13
Kevin Cogill <skwerl@gmail.com>
2013-01-12 09:55:20 -0800

    newsletter promo

--- modules/footerish.php
+++ modules/footerish.php
@@ -1,4 +1,9 @@
 <div id="footerish_thing" class="sidebar_module">
+
+	<div style="margin-bottom: 13px;">
+		<a href="/subscribe/"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/newsletter-promo.jpg" /></a>
+	</div>
+
 	<div class="header arvo">Contact</div>
 
 	<p>&copy; 2007-2012 Antiquiet, independently owned and operated. All audio and video content hosted with permission. Don't submit your music. We'll find you. Designed and built by <a href="http://iamskwerl.com">Skwerl</a>.</p>
