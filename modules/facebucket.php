ac7e305783d84030d6a90bebc36e465a95b976f2
Kevin Cogill <skwerl@gmail.com>
2013-01-12 09:55:59 -0800

    fixed firefox css issues, facebook activity box

--- modules/facebucket.php
+++ modules/facebucket.php
@@ -1,4 +1,13 @@
 <div id="facebook_stream" class="sidebar_module">
+
 	<div class="header arvo">Facebucket</div>
-	<div class="fb-activity" data-site="antiquiet.com" data-app-id="214912305229" data-width="300" data-height="300" data-header="false" data-border-color="#000" data-font="lucida grande" data-recommendations="false" style="background-color: #FFF9E5;"></div>
+
+	<div class="fb-activity" data-site="antiquiet.com" data-app-id="214912305229" data-width="300" data-height="300" data-header="false" data-linktarget="_top" data-font="lucida grande" data-recommendations="true" data-border-color="#000" style="background-color: #FFF9E5; width: 300px; height: 300px;"></div>
+	<style type="text/css">
+		div.fb-activity > span {
+			width: 300px !important;
+			height: 300px !important;
+		}
+	</style>
+
 </div>
