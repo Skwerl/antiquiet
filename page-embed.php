92859a43f453f4be51744592c6a1f999175e09f5
Kevin Cogill <skwerl@gmail.com>
2014-01-27 18:54:27 -0800

    fixed legacy video embeds

--- page-embed.php
+++ page-embed.php
@@ -3,35 +3,28 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <?php add_filter('show_admin_bar', '__return_false'); ?>
-<script type="text/javascript" src="http://cdn.antiquiet.com/jw-player/player/jwplayer.js"></script>
+<script type="text/javascript" src="http://p.jwpcdn.com/6/5/jwplayer.js?ver=3.8.1"></script>
 </head>
 <body style="margin: 0px; background-color: #000;">
+
+<style type="text/css">
+	p {
+		margin: 0px;
+		padding: 0px;
+	}
+</style>
+
 	<?php
 
 	$parent = get_post_field('post_parent', $_GET['media']);
 	$video_url = get_permalink($parent);
 	if ($video_url == get_permalink()) { $video_url = get_bloginfo('url'); }
 
-	$atts = array(
-		'width' => '560',
-		'height' => '315',
-		'stretching' => 'fill',
-		'controlbar' => 'bottom',
-		'mediaid' => $_GET['media'],
-		'sharing.link' => $video_url,
-		'plugins' => 'gapro-1,sharing-3',
-	#	'plugins' => 'gapro-1,sharing-3,ltas',
-	#	'ltas.cc' => 'kcibarzelnafsna',
-		'autostart' => false,
-		'logo.file' => 'http://cdn.antiquiet.com/jw-player/player/antiquiet.png',
-		'logo.link'=> 'http://www.antiquiet.com',
-		'logo.position' => 'bottom-right',
-		'logo.over' => '1',
-		'logo.out' => '0.8',
-		'logo.timeout' => '8'
-	);
-	echo fix_jw_embed(jwplayer_handler($atts));
-	
+	$content = '[jwplayer mediaid="'.$_GET['media'].'" width="560" height="315"]';
+	$formatted = apply_filters('the_content', $content);
+	echo $formatted;
+
 	?>
+
 </body>
 </html>
