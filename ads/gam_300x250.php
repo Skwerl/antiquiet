97974a1c814e8c9dff4a8fdae3470592840fe45d
Kevin Cogill <skwerl@gmail.com>
2014-02-24 23:33:21 -0800

    fixing ad slot handling

--- ads/gam_300x250.php
+++ ads/gam_300x250.php
@@ -1,19 +1,23 @@
-<?php if (has_tag('festivalfever') || has_category('festivals')) { ?>
-	<script type="text/javascript">
-	  GA_googleFillSlot("300x250_sponsor");
-	</script>
-<?php } elseif (is_home()) { ?>
-	<script type="text/javascript">
-	  GA_googleFillSlot("300x250_home");
-	</script>
-<?php } elseif (is_nsfw()) { ?>
+<?php if (is_nsfw()) { ?>
 	<div class="nsfw_ad_block" style="width: 300px; height: 250px;">
 		<div>I am not an advertisement because this content makes wholesome people uncomfortable.</div>
 	</div>
 <?php } else { ?>
-	<script type="text/javascript">
-	  GA_googleFillSlot("300x250_ros");
-	</script>
+
+	<?php if (is_home()) { ?>
+		<script type="text/javascript">
+		  GA_googleFillSlot("300x250_home");
+		</script>
+	<?php } elseif (has_tag('festivalfever') || has_category('festivals')) { ?>
+		<script type="text/javascript">
+		  GA_googleFillSlot("300x250_sponsor");
+		</script>
+	<?php } else { ?>
+		<script type="text/javascript">
+		  GA_googleFillSlot("300x250_ros");
+		</script>
+	<?php } ?>
+
 <?php } ?>
 
 <script type="text/javascript">		
--- ads/gam_728x90.php
+++ ads/gam_728x90.php
@@ -1,17 +1,21 @@
-<?php if (has_tag('festivalfever') || has_category('festivals')) { ?>
-	<script type="text/javascript">
-	  GA_googleFillSlot("728x90_sponsor");
-	</script>
-<?php } elseif (is_home()) { ?>
-	<script type="text/javascript">
-	  GA_googleFillSlot("728x90_home");
-	</script>
-<?php } elseif (is_nsfw()) { ?>
+<?php if (is_nsfw()) { ?>
 	<div class="nsfw_ad_block" style="width: 728px; height: 90px;">
 		<div>I am not an advertisement because this content makes wholesome people uncomfortable.</div>
 	</div>
 <?php } else { ?>
-	<script type="text/javascript">
-	  GA_googleFillSlot("728x90_ros");
-	</script>
+
+	<?php if (is_home()) { ?>
+		<script type="text/javascript">
+		  GA_googleFillSlot("728x90_home");
+		</script>
+	<?php } elseif (has_tag('festivalfever') || has_category('festivals')) { ?>
+		<script type="text/javascript">
+		  GA_googleFillSlot("728x90_sponsor");
+		</script>
+	<?php } else { ?>
+		<script type="text/javascript">
+		  GA_googleFillSlot("728x90_ros");
+		</script>
+	<?php } ?>
+
 <?php } ?>
